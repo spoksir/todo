cf98d132722440f132ef0021bcd9250ec8cb5b5c
spoksir <franciscovieira88@gmail.com>
2024-10-23 19:09:01 +0100

    Descreva suas alterações aqui

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div className='flex py-3 flex-col items-center'>
-        <h1 className='text-4xl font-bold py-10 text-blue-900'>To Do APP</h1>
+    return (
+      <div className='flex py-3 flex-col items-center'>
+        <h1 className='text-4xl font-bold py-10 text-blue-900 text-center'>To Do APP</h1>
         <h2 className='flex gap-[40px] text-gray-700 text-lg py-2'> Projeto - Francisco Vieira</h2>
-    </div>
-  )
-}
+      </div>
+    )
+  }
 
 export default Navbar
