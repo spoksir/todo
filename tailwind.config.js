ed55274d0c4642620f7cc2b5bdcb00e3062ff22b
spoksir <franciscovieira88@gmail.com>
2024-10-23 22:11:03 +0100

    commit do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Ajuste este caminho conforme necessário
-    "./public/index.html",
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
